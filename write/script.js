d988ccaad6a47fc3854d591f02c47a99eef791dd
Kwak Donghun <doghoon1@gmail.com>
2025-05-31 01:12:36 +0900

    í´§ Auto commit: 2025-05-31 01:12:36

--- write/script.js
+++ write/script.js
@@ -94,3 +94,20 @@ async function uploadToGitHub(token, repo, path, contentBase64, message) {
     }),
   });
 }
+
+
+  const preview = document.getElementById("preview");
+  document.getElementById("image").addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        preview.src = event.target.result;
+        preview.style.display = "block";
+      };
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "#";
+      preview.style.display = "none";
+    }
+  });
